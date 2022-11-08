d12136c90d9ab964fbf4455973e2bb5fefefe88c
Trần Hữu Phúc <30775322+Tranhuuphuc123@users.noreply.github.com>
2022-11-08 19:09:42 +0700

    luu thay doi 08112022

--- APTECH/EXECIRES/Java_oop/src/Nested_Ano/Khoitao.java
+++ APTECH/EXECIRES/Java_oop/src/Nested_Ano/Khoitao.java
@@ -1,5 +1,36 @@
-package Nested_Ano;
+package  Nested_Ano;
+//import thư viện Annotattion
 
-public class Khoitao {
+import java.lang.annotation.*;
+public class Khoitao{
+
+    // tiến hành khởi tạo các Annotation tự định nghĩa
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    //tạo tên pt Annotation tự định nghĩa
+    public @interface  phuc1 {
+        String value();
+    }
+
+    // khởi tạo Annotation thứ 2 để Nested Annotation t 1
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    public @interface phuc2{
+        phuc1[] value();
+    }
+    // tiến hành sử dụng tái định nghĩa lại bằng cách truyền value
+    // cho mảng Annotaion phuc1[]
+
+    @phuc2({
+            @phuc1("value1"),
+            @phuc1("value2")
+    })
+    public interface giaothuc{
+
+    }
+
+
 
 }
--- APTECH/EXECIRES/Java_oop/src/Nested_Ano/Man.java
+++ APTECH/EXECIRES/Java_oop/src/Nested_Ano/Man.java
@@ -1,4 +1,14 @@
-package Nested_Ano;
+package  Nested_Ano;
 
-public class Man {
+// tiến hành thực thi file Annotation vừa tạo
+public class Man extends Khoitao{
+    public static void main(String[] args){
+        // tiến hành Truy xuất chú thích qua lớp Bộ lọc
+
+        phuc2 ph = giaothuc.class.getAnnotation(phuc2.class);
+        for(phuc1 p: ph.value()){
+            System.out.println("xuất thông tin: " + p.value());
+        }
+
+    }
 }
