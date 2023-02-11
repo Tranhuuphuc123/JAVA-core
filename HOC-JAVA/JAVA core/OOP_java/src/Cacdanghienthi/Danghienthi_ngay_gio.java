93e63093f08dcc5157d8a8ddf8cffd9cf8096543
Trần Hữu Phúc <30775322+Tranhuuphuc123@users.noreply.github.com>
2023-02-12 00:09:24 +0700

    luu thay doi 02/12/2023

---
+++ HOC-JAVA/JAVA core/OOP_java/src/Cacdanghienthi/Danghienthi_ngay_gio.java
@@ -0,0 +1,8 @@
+package Cacdanghienthi;
+/*tiềm hiểu về dạng hển thị ngày và giờ trong java*/
+
+public class Danghienthi_ngay_gio{
+    public static void main(String[] args) {
+
+    }
+}
---
+++ HOC-JAVA/JAVA core/OOP_java/src/Exception_ThrowRuntimeException_CompileException/text.java
@@ -0,0 +1,8 @@
+package Exception_ThrowRuntimeException_CompileException;
+/*tiềm hiểu về throww runtimeException và compileException trong java*/
+public class text {
+    public static void main(String[] args) {
+
+    }
+
+}
